4089e27dc8a15a50641b52e0c12ce50a4789030b
Stephen Jacobs <stephenmjacobs@gmail.com>
2023-09-17 16:24:51 -0400

    Setup the priority queue for creating ships

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
+import java.util.PriorityQueue;
+import java.util.Comparator;
+
 public class Main {
+
     public static void main(String[] args)
     {
         Ship ship1 = ShipFactory.createShip(ShipFactory.BATTLESHIP);
-        System.out.println("Ship Created " + ship1.name + ":" + ship1.length);
         Ship ship2 = ShipFactory.createShip(ShipFactory.CRUISER);
-        System.out.println("Ship Created " + ship2.name + ":" + ship2.length);
         Ship ship3 = ShipFactory.createShip(ShipFactory.DESTROYER);
-        System.out.println("Ship Created " + ship3.name + ":" + ship3.length);
         Ship ship4 = ShipFactory.createShip(ShipFactory.SUBMARINE);
-        System.out.println("Ship Created " + ship4.name + ":" + ship4.length);
         Ship ship5 = ShipFactory.createShip(ShipFactory.CARRIER);
-        System.out.println("Ship Created " + ship5.name + ":" + ship5.length);
+
+        class ShipCompare implements Comparator<Ship> {
+
+            public int compare(Ship a, Ship b)
+            {
+                return (b.length - a.length);
+            }
+        }
+
+        PriorityQueue<Ship> ships = new PriorityQueue<>(new ShipCompare());
+        ships.add(ship1);
+        ships.add(ship2);
+        ships.add(ship3);
+        ships.add(ship4);
+        ships.add(ship5);
+
+        while(! ships.isEmpty()) {
+            Ship aShip = ships.remove();
+            System.out.println("Ship Created " + aShip.name + ":" + aShip.length);
+        }
+
     }
 }
