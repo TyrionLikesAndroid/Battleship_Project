fd39cb89381f69519cf08269cc048aebee8e49e9
Stephen Jacobs <stephenmjacobs@gmail.com>
2023-09-17 17:15:50 -0400

    Start flushing out the SimulationManager

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Comparator;
 
@@ -5,31 +6,11 @@ public class Main {
 
     public static void main(String[] args)
     {
-        Ship ship1 = ShipFactory.createShip(ShipFactory.BATTLESHIP);
-        Ship ship2 = ShipFactory.createShip(ShipFactory.CRUISER);
-        Ship ship3 = ShipFactory.createShip(ShipFactory.DESTROYER);
-        Ship ship4 = ShipFactory.createShip(ShipFactory.SUBMARINE);
-        Ship ship5 = ShipFactory.createShip(ShipFactory.CARRIER);
+        LinkedList<AttackStrategy> aList = new LinkedList<>();
+        aList.add(new SimpleAttackStrategy());
 
-        class ShipCompare implements Comparator<Ship> {
-
-            public int compare(Ship a, Ship b)
-            {
-                return (b.length - a.length);
-            }
-        }
-
-        PriorityQueue<Ship> ships = new PriorityQueue<>(new ShipCompare());
-        ships.add(ship1);
-        ships.add(ship2);
-        ships.add(ship3);
-        ships.add(ship4);
-        ships.add(ship5);
-
-        while(! ships.isEmpty()) {
-            Ship aShip = ships.remove();
-            System.out.println("Ship Created " + aShip.name + ":" + aShip.length);
-        }
+        SimulationManager mgr = new SimulationManager(new SimplePlacementStrategy(), aList);
+        mgr.runSimulation(1);
 
     }
 }
--- src/SimulationManager.java
+++ src/SimulationManager.java
@@ -6,15 +6,46 @@ import java.util.*;
  */
 public class SimulationManager {
 
+    class ShipCompare implements Comparator<Ship> {
 
-    private HashMap<String, BattleGrid> completedGames;
+        public int compare(Ship a, Ship b)
+        {
+            return (b.length - a.length);
+        }
+    }
 
-    public void SimulationManager(PlacementStrategy pStrat, LinkedList<AttackStrategy> aStrats) {
-        // TODO implement here
+    private HashMap<Integer, HashMap<String, BattleGrid>> totalGames;
+    private PlacementStrategy pStrat;
+    LinkedList<AttackStrategy> aStrats;
+
+    public SimulationManager(PlacementStrategy pStrat, LinkedList<AttackStrategy> aStrats) {
+        this.pStrat = pStrat;
+        this.aStrats = aStrats;
     }
 
     public void runSimulation(int numberOfGames) {
-        // TODO implement here
+
+        if(numberOfGames == 0)
+            return;
+
+        // Create the map for our total results of all runs
+        HashMap<String, BattleGrid> iterationGames = null;
+        totalGames = new HashMap<Integer, HashMap<String, BattleGrid>>();
+
+        for(int i=0; i< numberOfGames; i++)
+        {
+            // Create the array for our results for this iteration
+            iterationGames = new HashMap<String, BattleGrid>();
+
+            // Create the ships and the battle grid and give it to the placment strategy
+            // to beging ship placement
+            pStrat.placeShips(createShips(), createBattleGrid());
+
+            // SMJ - Do the attack loop
+
+            // Add this iteration game map into the master game map
+            totalGames.put(i, iterationGames);
+        }
     }
 
     public void exportResults(String filename) {
@@ -22,15 +53,30 @@ public class SimulationManager {
     }
 
     private PriorityQueue<Ship> createShips() {
-        return new PriorityQueue<Ship>();
+
+        PriorityQueue<Ship> ships = new PriorityQueue<>(new ShipCompare());
+
+        Ship ship = ShipFactory.createShip(ShipFactory.BATTLESHIP);
+        ships.add(ship);
+        ship = ShipFactory.createShip(ShipFactory.CRUISER);
+        ships.add(ship);
+        ship = ShipFactory.createShip(ShipFactory.DESTROYER);
+        ships.add(ship);
+        ship = ShipFactory.createShip(ShipFactory.SUBMARINE);
+        ships.add(ship);
+        ship = ShipFactory.createShip(ShipFactory.CARRIER);
+        ships.add(ship);
+
+        while(! ships.isEmpty()) {
+            ship = ships.remove();
+            System.out.println("Ship Created " + ship.name + ":" + ship.length);
+        }
+
+        return ships;
     }
 
     private BattleGrid createBattleGrid() {
         return new BattleGrid(10,10);
-    }
-
-    private void recordBattleResult(BattleGrid aGrid) {
-        // TODO implement here
     }
 
 }
